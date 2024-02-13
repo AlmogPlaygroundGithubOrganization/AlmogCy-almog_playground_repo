705b04ca97566e44e40018b04a3ae17103eaa746
AlmogCy <almog@cycode.com>
2024-02-13 15:42:04 +0200

    This commit added a python file with bad syntax and bad import packge

---
+++ python_file.py
@@ -0,0 +1,18 @@
+# Bad Python script with syntax errors and risky library import
+
+# Incorrect import statement with wrong module name
+import matplotlip.pyplot as plt
+
+# Syntax error: missing colon after defining a function
+def main()
+    print("This is a badly written Python script")
+    # Syntax error: missing quotation mark to close the string
+    print("It imports a library with potential security risks)
+    # Syntax error: missing parenthesis to close the print function call
+    print("It also has syntax errors")
+    # Attempting to use an undefined variable
+    print(undefined_variable)
+
+# Syntax error: missing call to the main function
+main
+
