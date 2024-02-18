3c95961bb735cd5fff33a0694c8043a66f936ec2
AlmogCy <almog@cycode.com>
2024-02-18 18:42:57 +0200

    files modifieddd

---
+++ Main.java
@@ -0,0 +1,11 @@
+import com.thoughtworks.xstream.XStream;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of XStream
+        XStream xstream = new XStream();
+        
+        // Your code using XStream...
+    }
+}
+
