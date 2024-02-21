f2e781f198dcfba12a5a00af1cae0b0977e76cdf
AlmogCy <almog@cycode.com>
2024-02-21 18:07:48 +0200

    filed modified

---
+++ passwords.py
@@ -0,0 +1,6 @@
+my_pass="981298461238964"
+print(f"This is my password", my_pass)
+
+api_access_key="ljasdlkjhasd"
+client_id="12894124-123123-123123-23325345"
+client_password="988aisdfihe1dfsf!!!@@"
